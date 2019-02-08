673fa640696437693f79be7ab8bd894b1b3f814e
Rachel A St Clair <rachel.aileen.st@gmail.com>
2019-02-08 15:11:17 -0500

    Add files via upload

---
+++ Search_Pad_Rows.py
@@ -0,0 +1,25 @@
+##looks through antibody database, pad 2 more rows and remove 300 columns to 256.
+
+import os
+from PIL import Image
+import numpy as np
+from skimage import io
+import imageio
+
+os.chdir('/home/whale/Desktop/Rachel/DeepProteins/AutoAntibodies/cyclegan/data/')
+counts = {}
+def pad_rows(folder):
+	files = os.listdir(folder)
+	for filename in files:
+		filepath = os.path.join(folder, filename)
+		im = np.asarray(Image.open(filepath)) # read in the image
+		im = im[:, :256, :]
+		#zeros = np.zeros([2, im.shape[1], 3], dtype=np.uint8)
+		#im = np.concatenate((im, zeros), axis=0)
+		imageio.imwrite(filepath, im)
+		print('meowzers')
+
+#pad_rows('testA')
+#pad_rows('testB')
+pad_rows('trainA')
+pad_rows('trainB')
