1af8a850feb92878b2fc1732c97aa35733fe34c1
Rachel A St Clair <rachel.aileen.st@gmail.com>
2020-02-27 16:19:09 -0500

    Add files via upload

--- lstm-pytorch.py
+++ lstm-pytorch.py
@@ -29,9 +29,10 @@ wandb.init(project='proteins_lstm')
 wab = wandb.config
 
 
-DATA_PATH = 'Bind_NOTBind' + '/'
+DATA_PATH = 'Data' + '/'
 
-wab.PURGE_LEN = 1000
+wab.PURGE_LEN_t = 1000
+wab.PURGE_LEN_v = 3000
 wab.DATA_SPLIT  = .3
 
 wab.NUM_CLASSES = 2
@@ -41,11 +42,11 @@ wab.HIDDEN_DIM = 100
 
 wab.LR = .001
 wab.BS = 16
-wab.NUM_EPOCHS = 25
+wab.NUM_EPOCHS = 100
 wab.OPTIM = 'adam'
 
 RESULTS = 'lstm/results'
-PRESAVE_NAME = RESULTS + ('/lstm-'+str(wab.NUM_EPOCHS)+'e-'+str(wab.LR)+'lr-'+str(wab.BS)+'bs-'+str(wab.HIDDEN_DIM)+'hd-'+str(wab.OPTIM)+'opt-'+str(wab.PURGE_LEN)+'max_len-'+str(wab.DATA_SPLIT)+'data_split-')
+PRESAVE_NAME = RESULTS + ('/lstm-'+str(wab.NUM_EPOCHS)+'e-'+str(wab.LR)+'lr-'+str(wab.BS)+'bs-'+str(wab.HIDDEN_DIM)+'hd-'+str(wab.OPTIM)+'opt-'+str(wab.PURGE_LEN_t)+'max_len-'+str(wab.PURGE_LEN_v)+'max_val_len-'+str(wab.DATA_SPLIT)+'data_split-'+'train-up-to-purge_lenV')
 
 
 
@@ -66,26 +67,37 @@ ba = wab.BS
 """load the data"""
 
 x = []
-with open(DATA_PATH + 'uniprot_data_batch_0_no_copies_bind.txt', 'r') as f:
+with open(DATA_PATH + 'AutoAntibodies/uniprot_data_batch_0.txt', 'r') as f:
     reader = csv.reader(f, delimiter=',')
     for row in reader:
         x.append(row)
 
 xnot = []
-with open(DATA_PATH + 'uniprot_data_batch_0_no_copies_not_bind.txt') as f:
+with open(DATA_PATH + 'Antibodies/uniprot_data_batch_0.txt') as f:
     reader = csv.reader(f, delimiter=',')
     for row in reader:
         xnot.append(row)
 
-def purge(X):
+
+def purge(X, purge_len):
     X = np.asarray(X)
     idxes_out = []
     X_lengths = [len(sentence) for sentence in X]
     for idx, x in enumerate(X_lengths):
-        if x > wab.PURGE_LEN:
+        if x > purge_len:
             idxes_out.append(idx)
     X = X[[i for i in range(int(X.shape[0])) if i not in idxes_out],...]
-    return X
+    return X, idxes_out
+
+def purge_min(X, purge_len):
+    X = np.asarray(X)
+    idxes_out = []
+    X_lengths = [len(sentence) for sentence in X]
+    for idx, x in enumerate(X_lengths):
+        if x < purge_len:
+            idxes_out.append(idx)
+    X = X[[i for i in range(int(X.shape[0])) if i not in idxes_out],...]
+    return X, idxes_out
 
 def hot_prots(X):
     X_bin = []
@@ -121,27 +133,41 @@ def load_batch(x, y):
     ins = torch.from_numpy(np.asarray(ins)).to(torch.float)
     return ins, labels, X_lengths
 
-x = purge(x)
-xnot = purge(xnot)
+
+x, _ = purge(x, wab.PURGE_LEN_v)
+xnot, _ = purge(xnot, wab.PURGE_LEN_v)
+
+print('Before balance X:', len(x), 'Xnot', len(xnot))
+
+rand_choice = np.random.choice(len(xnot), len(x))
+xnot = xnot[rand_choice]
+
+print('After balance X:', len(x), 'Xnot', len(xnot))
 
 y = np.ones(len(x))
 ynot = np.zeros((len(xnot)))
 
 X = np.append(x, xnot)
 Y = np.append(y, ynot)
+# np.random.shuffle(Y)
 
 X_lengths = [len(sentence) for sentence in X]
-pad_token = '26'
 longest = max(X_lengths)
 
 print('The average length is ', sum(X_lengths)//len(X_lengths), 'The longest sequence is ', longest, '!')
 plt.hist(X_lengths, bins=100)
 plt.title('Lengths of Sequences in dataset.')
-# plt.show()
+plt.show()
 
 X = hot_prots(X)
 
 x, y, xval, yval = split_train_val(X, Y)
+
+# x, idx_out = purge_min(x, wab.PURGE_LEN_t)
+# y = y[[i for i in range(y.shape[0]) if i not in idx_out]]
+
+print('Train data and labels size:', len(x), len(y), 'Val data and labels size:', len(xval), len(yval))
+
 
 data = {'train': [x,y], 'val': [xval,yval]}
 
@@ -231,14 +257,14 @@ def train():
 
 try:
     model, val_acc, val_loss, best_acc, time_elapsed = train()
-    duration = 3 #sound info
-    freq = 333 #more sound info
-    os.system('play -nq -t alsa synth {} sine {}'.format(duration, freq)) #play a sound when the program is finished
-
 except:
     print('keyboard interupt! :3')
-    SAVE_NAME = PRESAVE_NAME + str(best_acc.detach().cpu().numpy())
+    SAVE_NAME = PRESAVE_NAME
     torch.save(model, SAVE_NAME)
+
+duration = 3 #sound info
+freq = 400 #more sound info
+os.system('play -nq -t alsa synth {} sine {}'.format(duration, freq)) #play a sound when the program is finished
 
 
 
